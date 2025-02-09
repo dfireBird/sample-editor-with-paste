af3c5c6548248b7ffc71af51ede903a55aa0df0d
dfireBird <me@dfirebird.dev>
2025-02-09 19:40:04 +0530

    feat: initial layout of main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { ThemeProvider } from '@/components/theme-provider'
+
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 className='text-2xl font-medium'>Vite + React Init</h1>
-    </>
+    <ThemeProvider defaultTheme='dark'>
+      <div className='mx-auto w-full'>
+        <Tabs defaultValue='editor' className=''>
+          <TabsList className='grid w-full grid-cols-2 p'>
+            <TabsTrigger value='editor'>Editor</TabsTrigger>
+            <TabsTrigger value='preview'>Preview</TabsTrigger>
+          </TabsList>
+          <TabsContent value='editor'>Put Editor Content here</TabsContent>
+          <TabsContent value='preview'>Put Preview Content here</TabsContent>
+        </Tabs>
+      </div>
+    </ThemeProvider>
   )
 }
 
